850369290f22af2da6923c7505c3d84049ba3d37
David Flores <dcflores@live.com>
2023-07-15 12:45:45 -0700

    added routes to controllers

--- models/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const userRoutes = require('./userRoutes.js');
-const postRoutes = require('./postRoutes.js');
-
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
