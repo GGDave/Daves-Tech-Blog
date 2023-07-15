a6eb727fc0611798632097871623dc80d7f5bd1c
David Flores <dcflores@live.com>
2023-07-15 12:54:11 -0700

    Modified Models

--- models/Post.js
+++ models/Post.js
@@ -1,45 +1,46 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Posts extends Model {}
 
-Posts.init(
-  {
-    id: {
+class Post extends Model {}
+
+Post.init({
+  id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-          len: [1]
-      }
-    },
-    content: {
+      autoIncrement: true
+  },
+  title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
           len: [1]
       }
   },
-    user_id: {
+  content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          len: [1]
+      }
+  },
+  user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'project',
+          model: 'user',
+          key: 'id'
+      }
   }
-);
+}, {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'post'
+})
 
-module.exports = Project;
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,34 @@
 const User = require('./User');
-const Posts = require('./Post');
+const Post = require('./Post');
+const Comment = require('./Comment');
+//will add one at a time
 
-User.hasMany(Posts, {
-  foreignKey: 'user_id',
-});
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
 
-Posts.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
 
-module.exports = { User, Posts };
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = { User,
+    Post,
+    Comment
+};
