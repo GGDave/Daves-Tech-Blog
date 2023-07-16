7178d4c39da262f743a6a02a7bd5e40caf293b02
David Flores <dcflores@live.com>
2023-07-16 14:37:08 -0700

    added additional comments

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,17 @@
-const withAuth = (req, res, next) => {
+//The following function is a middleware function that will help whit authentication logic
+const withAuth = (req, res, next) => {//This is a middleware function that takes three parameters: req (the request object), 
+  //res (the response object), and next (a function that calls the next middleware in the stack).
+
+  //Inside the function, it checks if req.session.user_id exists. The req.session object is a way to store data that's accessible
+  // across multiple user requests (sessions). Here, user_id is being used to check whether the user is logged in.
   if (!req.session.user_id) {
-      res.redirect('/login');
+      res.redirect('/login');//If req.session.user_id does not exist (meaning the user is not logged in),
+      // res.redirect('/login') is called to redirect the user to the login page.
   } else {
-      next();
+      next();//If req.session.user_id does exist (meaning the user is logged in), next() is called, which allows the request to 
+      //proceed to the next middleware function (or to the route handler, if no more middleware functions are in the queue).
   }
 };
 
 module.exports = withAuth;
+//Using module export allows us to use this function in other parts of the code
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,14 @@
-function formatDate(date) {
+function formatDate(date) {//This function takes one argument, date and will return a string in the format "MM/DD/YYYY".
   return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
     ).getFullYear()}`;
+    //Inside the function, a new Date object is created from the input date. getMonth(), getDate(), and getFullYear() 
+    //methods are used on this Date object to extract the month, date, and year.
+    //getMonth() returns the month in the range 0-11, so 1 is added to the result to get the month in the range 1-12.
+    //getDate() returns the day of the month in the range 1-31.
+    //getFullYear() returns the year (four digits for dates between year 1000 and 9999).
 }
 
 
-module.exports = {
-  formatDate
-}
+module.exports = { formatDate}
+//allows us to export the helper and makes it available to the rest of the code
